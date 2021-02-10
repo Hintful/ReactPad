926eb780653c4897343e956cfc67aa7a14e5bdb6
Kurt Choi <choi.kurt3@gmail.com>
2021-02-09 23:09:00 -0500

    fix redirect issues

--- src/App.js
+++ src/App.js
@@ -146,7 +146,7 @@ class ReactPad extends React.Component {
           </div> */ }
         </div>
         <div class="credit">
-          Coded and designed by <a href="https://kurtchoi.dev" target="_blank">Kurt Choi</a>
+          Coded and designed by <u>Kurt Choi</u>
         </div>
       </div>
     ); 
